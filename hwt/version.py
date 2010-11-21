65ffc052ff8ddb4757e9c82d75241170dbbfb0aa
Patrick Marsh <patrickmarshwx@gmail.com>
2010-11-20 23:32:19 -0600

    Added fix to get the latest revision number. Added checks to see if git is installed.  If so, pull revision information.  If not check to see if __git_version__.py exists.  If so, read revision information.  (This will only exist for distributions, not when cloning git repository.)  If __git_version__.py doesn't exist and git not found, set revision to 'Unknown'.

--- hwt/version.py
+++ hwt/version.py
@@ -1,4 +1,5 @@
 import os.path
+import subprocess
 release = False
 __version__ = '0.1'
 
@@ -6,28 +7,59 @@ __version__ = '0.1'
 _repository_path = os.path.split(__file__)[0]
 _git_file_path = os.path.join(_repository_path, '__git_version__.py')
 
-def get_git_revision():
+
+def _minimal_ext_cmd(cmd):
+    # construct minimal environment
+    env = {}
+    for k in ['SYSTEMROOT', 'PATH']:
+        v = os.environ.get(k)
+        if v is not None:
+            env[k] = v
+    # LANGUAGE is used on win32
+    env['LANGUAGE'] = 'C'
+    env['LANG'] = 'C'
+    env['LC_ALL'] = 'C'
+    out = subprocess.Popen(cmd, stdout=subprocess.PIPE, env=env).communicate()[0]
+    return out
+
+def get_git_hash():
     '''
     Gets the last GIT commit hash and date for the repository, using the
     path to this file.
     '''
-    from subprocess import Popen, PIPE
-    rev = ''
     try:
-        proc = Popen(['git', 'log', '-1', '--date=short', '--format="%h %ad"', 
-                      _repository_path], stdout=PIPE)
-        text = proc.stdout.readline()
-        text = text.replace('"', '').split()
-        rev = '.dev.' + text[0] + ' (' + text[1] + ')'
-    except OSError:
-        # GIT not installed, don't worry about finding the revision hash
-        pass
+        out = _minimal_ext_cmd(['git', 'rev-parse', 'HEAD'])
+        GIT_REVISION = out.strip().decode('ascii')
+    except:
+        GIT_REVISION = None
+
+    return GIT_REVISION
+
+def get_git_revision():
+    hash = get_git_hash()
+    if hash :
+        rev = hash[:7]
+        try:
+            cmd = ['git', 'show', '%s' % (hash), '--date=short',
+                   '--format="(%ad)"']
+            date = _minimal_ext_cmd(cmd).split('"')[1]
+            rev += date
+        except:
+            pass
+    else:
+        rev = None
     return rev
 
 def write_git_version():
     'Write the GIT revision to a file.'
+    rev = get_git_revision()
+    if not rev:
+        if os.path.isfile(_git_file_path):
+            return
+        else:
+            rev = "Unknown"
     gitfile = open(_git_file_path, 'w')
-    gitfile.write('rev = "%s"\n' % get_git_revision())
+    gitfile.write('rev = "%s"\n' % rev)
     gitfile.close()
 
 
