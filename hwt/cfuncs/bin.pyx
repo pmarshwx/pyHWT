ffb11658416c755a85cf6eeea6d30ecd5fbb1791
Patrick Marsh <patrickmarshwx@gmail.com>
2010-11-21 23:01:26 -0600

    Initial add of regional_threshold code.  This is mainly a copy and paste of code from several other functions.  Not a working copy.

--- hwt/cfuncs/bin.pyx
+++ hwt/cfuncs/bin.pyx
@@ -36,3 +36,46 @@ def precip(np.ndarray[DTYPE2_t, ndim=2] var,
             
             
     return hist
+
+    
+@cython.boundscheck(False)
+@cython.cdivision(True)
+def regional_thresholds(np.ndarray[DTYPE_t, ndim=2] data, 
+                        np.ndarray[DTYPE2_t, ndim=2] mask,
+                        float roi, 
+                        float dx):
+    
+    cdef unsigned int ulength = data.shape[0]
+    cdef unsigned int vlength = data.shape[1]
+    cdef unsigned int ng
+    cdef int jw, je, isouth, inorth
+    cdef float rng, distsq, dist
+    cdef Py_ssize_t i, j, ii, jj
+
+    cdef np.ndarray[DTYPE_t, ndim=2] hitmiss = np.zeros([ulength, vlength], dtype=DTYPE)
+    cdef np.ndarray[DTYPE_t, ndim=2] tmphit = np.zeros([ulength, vlength], dtype=DTYPE)
+    
+    rng = roi/dx
+    ng = int(rng)
+    
+    for j in range(vlength):
+        for i in range(ulength):
+            jw = j-ng
+            je = j+ng + 1
+            isouth = i-ng
+            inorth = i+ng + 1
+            for jj in range(jw, je):
+                for ii in range(isouth, inorth):
+                    distsq = float(j-jj)**2 + float(i-ii)**2
+                    dist = distsq**0.5
+                    if dist <= rng:
+                        if jw < 0 or je >= vlength or isouth < 0 or inorth >= ulength:
+                            continue
+                        tmphit[ii,jj] = 1
+                        
+    return tmphit
+
+
+
+
+
