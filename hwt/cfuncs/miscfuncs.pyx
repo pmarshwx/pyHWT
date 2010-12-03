4262bcedcc145fe1d896ec63b920fbed330da74d
Patrick Marsh <patrickmarshwx@gmail.com>
2010-12-02 20:01:14 -0600

    Changed variable "cldice" to simply "ice".  Also changed the order of accepted variables in definition.  Variables are now accepted in the order in which they are checked in the routine.

--- hwt/cfuncs/miscfuncs.pyx
+++ hwt/cfuncs/miscfuncs.pyx
@@ -16,11 +16,11 @@ ctypedef np.float32_t DTYPE32_t
 
 
 @cython.boundscheck(False)
-def ptype(np.ndarray[DTYPE2_t, ndim=2] cloud,
-          np.ndarray[DTYPE2_t, ndim=2] rain,
+def ptype(np.ndarray[DTYPE2_t, ndim=2] rain,
           np.ndarray[DTYPE2_t, ndim=2] snow,
-          np.ndarray[DTYPE2_t, ndim=2] cldice,
           np.ndarray[DTYPE2_t, ndim=2] graupel,
+          np.ndarray[DTYPE2_t, ndim=2] cloud,
+          np.ndarray[DTYPE2_t, ndim=2] ice,
           np.ndarray[DTYPE2_t, ndim=2] t2m):
 
     cdef unsigned int ii = cloud.shape[0]
@@ -33,7 +33,7 @@ def ptype(np.ndarray[DTYPE2_t, ndim=2] cloud,
         for j in range(jj):
             # Is rain the largest
             if (rain[i,j] > cloud[i,j] and rain[i,j] > snow[i,j] and 
-                rain[i,j] > cldice[i,j] and rain[i,j] > graupel[i,j]):
+                rain[i,j] > ice[i,j] and rain[i,j] > graupel[i,j]):
                     if t2m[i,j] > 273.15:
                         ptype[i,j] = 1
                     # Check for Freezing Rain
@@ -41,26 +41,26 @@ def ptype(np.ndarray[DTYPE2_t, ndim=2] cloud,
                         ptype[i,j] = 5
             # Is snow the largest
             elif (snow[i,j] > cloud[i,j] and snow[i,j] > rain[i,j] and 
-                  snow[i,j] > cldice[i,j] and cloud[i,j] > graupel[i,j]):
+                  snow[i,j] > ice[i,j] and cloud[i,j] > graupel[i,j]):
                     ptype[i,j] = 2
             
             # Is graupel the largest
             elif (graupel[i,j] > cloud[i,j] and graupel[i,j] > rain[i,j] and 
-                  graupel[i,j] > snow[i,j] and graupel[i,j] > cldice[i,j]):
+                  graupel[i,j] > snow[i,j] and graupel[i,j] > ice[i,j]):
                     ptype[i,j] = 3
 
             # Is cloud the largest
             elif (cloud[i,j] > rain[i,j] and cloud[i,j] > snow[i,j] and 
-                  cloud[i,j] > cldice[i,j] and cloud[i,j] > graupel[i,j]):
+                  cloud[i,j] > ice[i,j] and cloud[i,j] > graupel[i,j]):
                   if t2m[i,j] > 273.15:
                       ptype[i,j] = 6
                   # Check for Freezing Fog
                   else:
                       ptype[i,j] = 7
 
-            # Is cldice the largest
-            elif (cldice[i,j] > cloud[i,j] and cldice[i,j] > rain[i,j] and 
-                  cldice[i,j] > snow[i,j] and cldice[i,j] > graupel[i,j]):
+            # Is ice the largest
+            elif (ice[i,j] > cloud[i,j] and ice[i,j] > rain[i,j] and 
+                  ice[i,j] > snow[i,j] and ice[i,j] > graupel[i,j]):
                     ptype[i,j] = 8
                     
             # Is any of the rain, snow, grapuel equal
@@ -72,9 +72,9 @@ def ptype(np.ndarray[DTYPE2_t, ndim=2] cloud,
                     else:
                             ptype[i,j] = 4
                     
-            # Is cloud equal to cldice
-            elif (cloud[i,j] == cldice[i,j]):
-                    if (cloud[i,j] == 0 and cldice[i,j] == 0):
+            # Is cloud equal to ice
+            elif (cloud[i,j] == ice[i,j]):
+                    if (cloud[i,j] == 0 and ice[i,j] == 0):
                         ptype[i,j] = 0
                     else:
                         ptype[i,j] = 8
