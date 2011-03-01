19b365851218816fcd6c297b1a95d6e346389839
Patrick Marsh <patrickmarshwx@gmail.com>
2011-03-01 16:39:39 -0600

    Rewrite of the anisotropic gaussian code. Limited testing shows this routine to work.

--- hwt/cfuncs/smoothers.pyx
+++ hwt/cfuncs/smoothers.pyx
@@ -6,6 +6,8 @@ cimport cython
 
 cdef extern from 'math.h':
     float exp(float x)
+    float cos(float x)
+    float sin(float x)
 
 
 DTYPE64 = np.float64
@@ -74,36 +76,88 @@ def gaussian(np.ndarray[DTYPE64_t, ndim=2] data,
 
 @cython.boundscheck(False)
 @cython.cdivision(True)
-def generic_smoother(np.ndarray[DTYPE64_t, ndim=2] data, 
-                     np.ndarray[DTYPE64_t, ndim=2] smoother):
+def anisotropic_gauss(np.ndarray[DTYPE64_t, ndim=2] data, 
+                      float sigx, float sigy, float rot, 
+                      int h, int k, float dx, float factor, 
+                      sig_as_grid_points=False):
 
-    cdef unsigned int ulength = data.shape[0]
-    cdef unsigned int vlength = data.shape[1]
-    cdef unsigned int nx, ny, hnx, hny
-    cdef int nhnx, nhny, iii, jjj
-    cdef Py_ssize_t i, j, ii, jj
-
-    cdef np.ndarray[DTYPE64_t, ndim=2] frc_data = np.zeros_like(data)
-
-    nx = smoother.shape[0]
-    ny = smoother.shape[0]
-    hnx = int(float(nx/2) + 0.5)
-    nhnx = -1 * hnx
-    hny = int(float(ny/2) + 0.5)
-    nhny = -1 * hny
+    cdef unsigned int vlength = data.shape[0]
+    cdef unsigned int ulength = data.shape[1]
+    cdef unsigned int a, b, nxy, pwdimlength
+    cdef int nxyn, iw, ie, js, jn, nw
+    cdef float sigx_sq, sigy_sq, rad, amp
+    cdef float sintheta, sintheta_sq, costheta, costheta_sq
+    cdef float A, B, C, D, E, F, ellipse, X, Y
+    cdef Py_ssize_t i, j, ii, jj, x, y
     
+    cdef np.ndarray[DTYPE64_t, ndim=2] frc_data = np.zeros([vlength, ulength], dtype=DTYPE64)
+    cdef float PI=3.141592653589793
 
-    for i in range(0, ulength):
-        for j in range(0, vlength):
-            if data[i,j] > 0:
-                for ii in range(nhnx, hnx):
-                    for jj in range(nhny, hny):
-                        iii = i + ii + nhnx
-                        jjj = j + jj + nhny
-                        if jjj < 0 or jjj >= vlength or iii < 0 or iii >= ulength:
-                            continue
-                        frc_data[iii,jjj] += data[i,j]*smoother[ii,jj]
+    if not sig_as_grid_points:
+        sigx = float(sigx / dx)
+        sigy = float(sigy / dx)
 
+    sigx_sq = sigx*sigx
+    sigy_sq = sigy*sigy    
+
+    # Set up X-axis
+    a = int(factor * sigx)
+    a_sq = float(a * a)
+
+    # Set up Y-axis
+    b = int(factor * sigy)
+    b_sq = float(b * b)
+
+    # Set weight-loop to size of maximum length
+    if a >= b:
+        nxy = a
+    else: 
+        nxy = b
+    
+    nxyn = -1 * nxy
+    pwdimlength = 2*nxy+1
+
+    # Rotation Matrix Values
+    rad = rot * PI / 180. * -1.   # Not entirely sure why the -1, but it makes it work
+    sintheta = sin(rad)
+    sintheta_sq = sintheta**2
+    costheta = cos(rad)
+    costheta_sq = costheta**2
+
+    # Constants used in transformation
+    A = (costheta_sq / a_sq) + (sintheta_sq / b_sq)
+    B = -2 * costheta * sintheta * ((1 / a_sq) - (1 / b_sq))
+    C = (sintheta_sq / a_sq) + (costheta_sq / b_sq)
+    # D = (2 * A * h ) + (B * k)
+    # E = (2 * C * k) + (B * h)
+    # F = (A * h * h) + (B * h * k) + (C * k *  k)
+
+    cdef np.ndarray[DTYPE64_t, ndim=1] partweight = np.zeros([pwdimlength**2], dtype=DTYPE64)
+    
+    nw = -1
+    for y in range(nxyn, nxy+1):
+     for x in range(nxyn, nxy+1):
+         nw += 1
+         ellipse = (A*x*x) + (B*x*y) + (C*y*y) # - D*X) - (E*Y) + F
+         if ellipse <= 1:
+             X = (x)*costheta - (y)*sintheta
+             Y = (x)*sintheta + (y)*costheta
+             partweight[nw] = exp(-0.5*((X*X / sigx_sq) + (Y*Y / sigy_sq))) 
+
+    for j in range(0, vlength):
+     for i in range(0, ulength):
+         if data[i,j] > 0:
+             amp = data[i,j] / (2*PI*sigx*sigy)
+             iw=i-nxy
+             ie=i+nxy
+             js=j-nxy
+             jn=j+nxy
+             nw = -1
+             for jj in range(js+k, jn+k+1):
+                 for ii in range(iw+h, ie+h+1):
+                     nw += 1
+                     if jj < 0 or jj >= vlength or ii < 0 or ii >= ulength:
+                         continue
+                     frc_data[jj,ii] = frc_data[jj,ii] + amp*partweight[nw]
 
     return frc_data
-
