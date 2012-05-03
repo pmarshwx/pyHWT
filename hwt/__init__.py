b7b3718003bc276fc4815e8f20975e7866bd5798
Patrick Marsh <patrick.marsh@noaa.gov>
2012-05-03 15:37:46 -0500

    Add object code to __init__

--- hwt/__init__.py
+++ hwt/__init__.py
@@ -2,4 +2,6 @@ import version
 import plot
 from cfuncs import *
 
+__all__ = ["cfuncs"]
+
 __version__ = version.get_version()
--- hwt/cfuncs/__init__.py
+++ hwt/cfuncs/__init__.py
@@ -4,3 +4,7 @@ import verification
 import smoothers
 import miscfuncs
 import ensemble
+import objects
+
+__all__ = ["bin", "neighborhood", "verification", "smoothers", "miscfuncs",
+           "ensemble", "objects"]
